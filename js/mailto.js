32f7441180f1b1e77a90b215a582ef96f32ccdbb
Cem Eren Şentürk <senturkcemeren@gmail.com>
2023-03-18 10:32:33 +0300

    Add files via upload

---
+++ js/mailto.js
@@ -0,0 +1,23 @@
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  // Check if all fields are filled
+  if (!name || !email || !message) {
+    const error = document.createElement("div");
+    error.classList.add("error");
+    error.textContent = "All fields are required.";
+    form.appendChild(error);
+    return;
+  }
+
+  // Create the email message
+  const emailMessage = `Name: ${name}\nEmail: ${email}\n\n${message}`;
+
+  // Open the user's email client and populate the fields
+  window.location.href = `mailto:info@planrota.com?subject=Contact Us&body=${emailMessage}`;
+});
